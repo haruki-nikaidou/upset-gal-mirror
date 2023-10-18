209ada59d29ab5b570f3d8358dbef482155e32fa
haruki <meteo.haru@gmail.com>
2023-10-18 06:02:50 +0100

    impl Button Ratio

--- src/pages/Mobile/Mobile.tsx
+++ src/pages/Mobile/Mobile.tsx
@@ -5,12 +5,30 @@ import "../../style/glass.css";
 import List from "../../components/List/List.tsx";
 import Search from "../../components/Search/Search.tsx";
 import Logo from "../../components/Logo/Logo.tsx";
+import ButtonRatio from "../../components/ButtonRatio/ButtonRatio.tsx";
 
 const Mobile: Component = () => {
     return (
         <>
             <Logo/>
             <div class={`glass ${styles.mobileContainer}`}>
+                <ButtonRatio selected={0} items={
+                    [
+                        {
+                            title: "Kirikiri 2",
+                        },
+                        {
+                            title: "APK",
+                        },
+                        {
+                            title: "ones",
+                        },
+                        {
+                            title: "Artroid",
+                        }
+                    ]
+                }
+                />
                 <Search/>
                 <List items={
                     [
