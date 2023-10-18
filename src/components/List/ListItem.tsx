443ea272a1ac7980e7dd98d09d1b42af9af6163c
haruki <meteo.haru@gmail.com>
2023-10-18 15:05:53 +0100

    add feather: add event listener for list items

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -5,16 +5,16 @@ export interface ListItemProps {
     title: string;
     size: string;
     resourceType: string;
-    onClick?: () => void;
-    onContextMenu?: () => void;
+    onClick?: (event: MouseEvent, item: ListItemProps ) => void;
+    onContextMenu?: (event: MouseEvent, item: ListItemProps) => void;
 }
 
 const ListItem: Component<ListItemProps> = (props) => {
     const {title, size, resourceType} = props;
     return (
         <div class={`${styles.listItem}`}
-                onClick={props.onClick}
-                onContextMenu={props.onContextMenu}
+                onClick={(e) => props.onClick?.(e, props) }
+                onContextMenu={(e) => props.onContextMenu?.(e, props)}
         >
             <p class={`${styles.title}`}>{title}</p>
             <p class={`${styles.info}`}>大小：{size}，类型：{resourceType}</p>
